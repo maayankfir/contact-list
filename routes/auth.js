9de07a3f6d852d511fdd99a8cbf7b854074ff7c4
Maayan Kfir <mayankfir@gmail.com>
2019-09-18 11:31:58 -0400

    contacts get all and post new

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,19 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-
+const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
 router.post(
